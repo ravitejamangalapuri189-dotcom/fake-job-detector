862ebff27ac7a49b0745914c65881bd1dabbd4d4
Raviteja mangalapuri <ravitejamangalapuri189@gmail.com>
2026-07-24 11:10:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,7 @@ def predict():
         prediction=result,
         reasons=found
     )
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
